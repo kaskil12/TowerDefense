576d701f07aa17ebf4e1a347929159fdcfa4b398
Kasper Kilde <kasperkilde2@gmail.com>
2025-02-12 21:28:31 +0100

    Uploaded game again

---
+++ 2DTowerPlaystore/appbundleV6_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/UnityEngine.GridModule_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void Grid_get_cellSize_m5512593532CABA9CFC058123B923AFA483D6003E (void);
+extern void Grid_get_cellGap_m9B12CA3DA5A7AC906A1E1943FABD3E1A523439A1 (void);
+extern void Grid_get_cellSize_Injected_mAA6A2B2F7C979279AEC73D5D82A20851E3F11A9B (void);
+extern void Grid_get_cellGap_Injected_m9613E7B62D3FC53FB02D6EF8A6E128E80601ED65 (void);
+extern void GridLayout_CellToLocalInterpolated_mE1FC35F36111BD0881573C6F51C37239BF3BD621 (void);
+extern void GridLayout_WorldToCell_m72AFBAA2458CD70A302708AA09DC2641BD95E21E (void);
+extern void GridLayout_LocalToWorld_m4714A4DAF4FF9545E9CCB3A8ACB5731A498D9CA6 (void);
+extern void GridLayout_DoNothing_mA280987BF98D257023D46C2C01902FC82EE6A00A (void);
+extern void GridLayout_CellToLocalInterpolated_Injected_mB16B7B8062F5DF8F78BF270B9D5E7B6BA8CEB3EE (void);
+extern void GridLayout_WorldToCell_Injected_mE067D9FE59A86E32CBBAD7EE56D9AE335229C606 (void);
+extern void GridLayout_LocalToWorld_Injected_mBF494266A8A287F7076B07952A03AFD0BAC008C0 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	Grid_get_cellSize_m5512593532CABA9CFC058123B923AFA483D6003E,
+	Grid_get_cellGap_m9B12CA3DA5A7AC906A1E1943FABD3E1A523439A1,
+	Grid_get_cellSize_Injected_mAA6A2B2F7C979279AEC73D5D82A20851E3F11A9B,
+	Grid_get_cellGap_Injected_m9613E7B62D3FC53FB02D6EF8A6E128E80601ED65,
+	GridLayout_CellToLocalInterpolated_mE1FC35F36111BD0881573C6F51C37239BF3BD621,
+	GridLayout_WorldToCell_m72AFBAA2458CD70A302708AA09DC2641BD95E21E,
+	GridLayout_LocalToWorld_m4714A4DAF4FF9545E9CCB3A8ACB5731A498D9CA6,
+	GridLayout_DoNothing_mA280987BF98D257023D46C2C01902FC82EE6A00A,
+	GridLayout_CellToLocalInterpolated_Injected_mB16B7B8062F5DF8F78BF270B9D5E7B6BA8CEB3EE,
+	GridLayout_WorldToCell_Injected_mE067D9FE59A86E32CBBAD7EE56D9AE335229C606,
+	GridLayout_LocalToWorld_Injected_mBF494266A8A287F7076B07952A03AFD0BAC008C0,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	15206,
+	15206,
+	21949,
+	21949,
+	10293,
+	10298,
+	10293,
+	15217,
+	19841,
+	19841,
+	19841,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_GridModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_GridModule_CodeGenModule = 
+{
+	"UnityEngine.GridModule.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
